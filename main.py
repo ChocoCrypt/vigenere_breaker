3bb70a92cafb2397f80f563e4ee61517588190c3
Rootdrigo <errecastillo@gmail.com>
2021-08-24 15:24:09 -0400

    superdone

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from methods import *
 
 
+#main -open string,  decode it, save it.
 if __name__ == '__main__':
     with open("encoded.txt") as file:
         encoded = file.read().replace("\n" , "")
-
     print(f"input: {encoded}")
     strings = get_key_lenght_text(encoded)
     decoded_strings = adecuated_string_to_decoded_strings(strings)
     decoded = reverse_decoded_strings(decoded_strings)
-
-
     with open("decoded.txt" , "w") as file:
         file.write(decoded)
     print(f"\noutput : {decoded}")
-
